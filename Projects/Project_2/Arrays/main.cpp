3025f5c13b351dcd62f6c1ea1b995a7592c0b2d7
Christopher Mathis <59633637+ChrisMathis98@users.noreply.github.com>
2020-02-08 09:32:54 -0800

    update

---
+++ Projects/Project_2/Arrays/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * Author: Christopher Mathis
+ * Created: February 5, 2020
+ * Purpose: Binary Search
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - No Global Variables
+//Only Universal Constants, Math, Physics, Conversions, Higher Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Set Random Number seed
+    
+    //Declare Variable Data Types and Constants
+    
+    //Initialize Variables
+   
+    //Process or map Inputs to Outputs
+    
+    //Display Outputs
+    
+    //Exit stage right!
+    return 0;
+}
